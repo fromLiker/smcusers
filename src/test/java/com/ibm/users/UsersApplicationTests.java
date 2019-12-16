bb8986ca907422ecfbdff0f5777cad011896c9b0
xuexp <xuexp@cn.ibm.com>
2019-12-16 17:53:27 +0800

    remove test import

--- src/test/java/com/ibm/users/UsersApplicationTests.java
+++ src/test/java/com/ibm/users/UsersApplicationTests.java
@@ -1,15 +1,15 @@
 package com.ibm.users;
 
-import org.junit.Test;
+// import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.ibm.users.entity.Userinfolist;
-import com.ibm.users.service.UsersService;
+// import com.ibm.users.entity.Userinfolist;
+// import com.ibm.users.service.UsersService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
